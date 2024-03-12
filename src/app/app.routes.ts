dce24733d034e1dbb3e72d64f9a5b313cc8664f4
Celiz <maticelizramos@gmail.com>
2024-03-11 22:12:50 -0300

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
 import { TodoComponent } from './components/todo/todo.component';
-import { CalendarComponent } from './components/calendar/calendar.component';
+
 
 export const routes: Routes = [
   {
     path: 'todo',
     component: TodoComponent,
   },
-  {
-    path: 'calendar',
-    component: CalendarComponent,
-  },
-  
+
   {
     path: '**',
     redirectTo: 'todo',
